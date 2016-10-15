10b0d1b47fc7e447b75b435fb6664dc5a6261294
MariusVin <m.vinickis@gmail.com>
2016-10-15 03:45:13 +0100

    More tears with swift, I can see the light at the end of the tunnel.

--- ios/GUTS2016/GUTS2016/controlView.swift
+++ ios/GUTS2016/GUTS2016/controlView.swift
@@ -9,25 +9,100 @@
 import Foundation
 import UIKit
 
+// Defining YellowBar class
+class YellowBarView: UIImageView {
+    let yellowBarView = UIImageView(image: #imageLiteral(resourceName: "yellowBar"))
+    init() {
+        super.init(image: #imageLiteral(resourceName: "yellowBar"))
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+class GreenBarView: UIImageView {
+    let yellowBarView = UIImageView(image: #imageLiteral(resourceName: "greenBar"))
+    init() {
+        super.init(image: #imageLiteral(resourceName: "greenBar"))
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
 
 class ControlView: UIView {
-    
-    let image = UIImage(named: "battery")
     var imageView: UIImageView
-    let viewTitle = UILabel()
-    
+    var redBarView: UIImageView
     override init(frame: CGRect) {
-        imageView = UIImageView(image: image)
-        
+        imageView = UIImageView(image: #imageLiteral(resourceName: "battery"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        redBarView = UIImageView(image: #imageLiteral(resourceName: "redBar"))
+        redBarView.translatesAutoresizingMaskIntoConstraints = false
+        
         
         super.init(frame: frame)
         
         self.backgroundColor = MerithayanUI.blueGrey.base
         
+        // Defining red bar
+        imageView.addSubview(redBarView)
+        imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-3.5-[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":redBarView]))
+        imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-35-[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":redBarView]))
+   
+        // Defining yellow bar
+        for _ in 0...3 {
+            let yellowBarView = YellowBarView()
+            yellowBarView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.addSubview(yellowBarView)
+        }
+        
+        
+        // Defining green bar
+        for _ in 0...8 {
+            let greenBarView = GreenBarView()
+            greenBarView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.addSubview(greenBarView)
+        }
+        
+        
+
+        
+        let viewArray = imageView.subviews
+        var counter = 0
+        
+        // Adding yellow bar constraints
+        for _ in viewArray {
+            if counter == 0 {
+                imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0][v1]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":redBarView, "v1":viewArray[counter+1]]))
+            } else if counter > 0 && counter < 5 {
+                
+                imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0][v1]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":viewArray[counter-1], "v1":viewArray[counter]]))
+            } else if counter == 5 {
+                imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0][v1]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":viewArray[counter-1], "v1":viewArray[counter]]))
+            } else {
+                
+                imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v0][v1]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":viewArray[counter-1], "v1":viewArray[counter]]))
+            }
+            
+            imageView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-3.5-[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":viewArray[counter]]))
+            counter += 1
+            
+        }
+        
+        // Adding green bar constraints
+        for _ in viewArray {
+            
+        }
+        
+        
+        
+    
+        
         self.addSubview(imageView)
+        
         self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":imageView]))
         self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":imageView]))
 
